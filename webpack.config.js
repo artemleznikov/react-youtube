e003b47b0ef0d73c4b1fbc8a4d5ab952e56fb982
Artem Leznikov <artemleznikov@gmail.com>
2018-05-26 17:27:35 +0000

    init

--- webpack.config.js
+++ webpack.config.js
@@ -20,11 +20,13 @@ module.exports = {
     extensions: ['', '.js', '.jsx']
   },
   devServer: {
+    host: "0.0.0.0",
+    port: 3000,
     historyApiFallback: true,
     contentBase: './',
     watchOptions: {
       aggregateTimeout: 300,
-      poll: 1000
+      poll: true
     }
   }
 };
